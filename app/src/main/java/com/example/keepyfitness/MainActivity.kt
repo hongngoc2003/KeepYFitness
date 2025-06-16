8b3ded322f36c2c74174800e2f5a0a187da7386c
hongngoc2003 <nhngoc1506@gmail.com>
2025-06-17 01:06:22 +0700

    camera permission

--- app/src/main/java/com/example/keepyfitness/MainActivity.kt
+++ app/src/main/java/com/example/keepyfitness/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.keepyfitness
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,5 +18,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //TODO ask for permission of camera upon first launch of application
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+            ) {
+                val permission = arrayOf(
+                    android.Manifest.permission.CAMERA
+
+                )
+                requestPermissions(permission, 1122)
+            } else {
+                //TODO show live camera footage
+
+            }
+        }else{
+            //TODO show live camera footage
+
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //TODO show live camera footage
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //TODO show live camera footage
+
+        } else {
+            finish()
+        }
+    }
+
 }
