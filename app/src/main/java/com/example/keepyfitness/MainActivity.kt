d05566ca9fd4c09a47554ce3dadfda587bf9b94b
hongngoc2003 <nhngoc1506@gmail.com>
2025-06-18 01:54:49 +0700

    live camera

--- app/src/main/java/com/example/keepyfitness/MainActivity.kt
+++ app/src/main/java/com/example/keepyfitness/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.keepyfitness
 
 import android.content.pm.PackageManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Size
+import android.media.ImageReader
+import android.content.Context
+import android.media.ImageReader.OnImageAvailableListener
+import android.view.Surface
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnImageAvailableListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,11 +38,53 @@ class MainActivity : AppCompatActivity() {
                 requestPermissions(permission, 1122)
             } else {
                 //TODO show live camera footage
+                setFragment()
 
             }
         }else{
             //TODO show live camera footage
+            setFragment()
+        }
+    }
 
+    var previewHeight = 0;
+    var previewWidth = 0
+    var sensorOrientation = 0;
+    //TODO fragment which show llive footage from camera
+    protected fun setFragment() {
+        val manager =
+            getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        var cameraId: String? = null
+        try {
+            cameraId = manager.cameraIdList[1]
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+        val fragment: Fragment
+        val camera2Fragment = CameraConnectionFragment.newInstance(
+            object :
+                CameraConnectionFragment.ConnectionCallback {
+                override fun onPreviewSizeChosen(size: Size?, rotation: Int) {
+                    previewHeight = size!!.height
+                    previewWidth = size.width
+                    sensorOrientation = rotation - getScreenOrientation()
+                }
+            },
+            this,
+            R.layout.camera_fragment,
+            Size(640, 480)
+        )
+        camera2Fragment.setCamera(cameraId)
+        fragment = camera2Fragment
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+    }
+
+    protected fun getScreenOrientation(): Int {
+        return when (windowManager.defaultDisplay.rotation) {
+            Surface.ROTATION_270 -> 270
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_90 -> 90
+            else -> 0
         }
     }
 
@@ -47,10 +97,14 @@ class MainActivity : AppCompatActivity() {
         //TODO show live camera footage
         if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             //TODO show live camera footage
-
+            setFragment()
         } else {
             finish()
         }
+    }
+
+    override fun onImageAvailable(p0: ImageReader?) {
+        p0?.acquireLatestImage()?.close()
     }
 
 }
